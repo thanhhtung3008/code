0d369ef9dd8d39bbad4e5eb568a61bc146a96ae2
thanhhtung3008 <thanhhtung3008@gmail.com>
2024-08-10 16:54:53 +0700

    new

---
+++ defines.h
@@ -0,0 +1,12 @@
+#define ft first
+#define sc second
+#define ll long long
+#define pll pair<ll, ll>
+#define FOR(i, l, r) for (ll i = (l); i <= (r); ++i)
+#define FORD(i, r, l) for (ll i = (r); i >= (l); --i)
+
+const ll maxn = 1e6 + 7;
+const ll N = 2e5 + 7;
+const long long oo = 1e18 + 7;
+const long long INF = 0x3f;
+const long long MOD = 1e9 + 7;
